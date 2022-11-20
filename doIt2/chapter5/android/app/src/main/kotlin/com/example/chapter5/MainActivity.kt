8ba18e07d0cd0f265e1e098692a130b35607b0d7
farmerkyh <farmerkyh@naver.com>
2022-11-20 20:56:43 +0900

    .

---
+++ doIt2/chapter5/android/app/src/main/kotlin/com/example/chapter5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- doIt2/chapter5/lib/main.dart
+++ doIt2/chapter5/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'dart:ffi' as ffi;
-import 'package:flutter/foundation.dart';
 
 import 'animalItem.dart';
-import 'cupertinoMain.dart';
 import 'sub/firstPage.dart';
 import 'sub/secondPage.dart';
 
@@ -14,25 +11,37 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    debugPrint("asdfasdfasdf================");
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-
-      //iPhone Design 화면
-      //home: CupertinoMain(),
-
-      //Android Design 화면
-      home: MyHomePage(title: "안드스타일"),
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-//------------------------------------------------------
-// android용 Design
-//------------------------------------------------------
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
@@ -47,15 +56,6 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   void initState() {
     super.initState();
-
-    // debugPrint("$Platform.isIOS");
-    // if (Platform.isIOS) {
-    //   //iOS
-
-    // } else if (Platform.isAndroid) {
-    //   //Android
-    // }
-
     controller = TabController(length: 2, vsync: this);
     animalList.add(
         Animal(animalName: "벌", kind: "곤충", imagePath: "repo/images/bee.png"));
@@ -79,19 +79,23 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Listview Example'),
+          title: Text('Listview Example'),
         ),
         body: TabBarView(
-          controller: controller,
           children: <Widget>[
             FirstApp(list: animalList),
             SecondApp(list: animalList)
           ],
+          controller: controller,
         ),
         bottomNavigationBar: TabBar(
-          tabs: const <Tab>[
-            Tab(icon: Icon(Icons.home, color: Colors.blue)),
-            Tab(icon: Icon(Icons.add, color: Colors.blue))
+          tabs: <Tab>[
+            Tab(
+              icon: Icon(Icons.looks_one, color: Colors.blue),
+            ),
+            Tab(
+              icon: Icon(Icons.looks_two, color: Colors.blue),
+            )
           ],
           controller: controller,
         ));
